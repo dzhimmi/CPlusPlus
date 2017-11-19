a8115f9a41297ee433c9c285b1c758b661660f3a
Hjalte Korsgaard Hansen <Boffen1990@gmail.com>
2017-11-19 03:05:28 +0100

    Playing with limits and types.

---
+++ TutorialsAndTribulations/testsource.cpp
@@ -0,0 +1,25 @@
+//Includes
+#include <iostream>
+#include <string>
+#include <limits>
+
+
+//Misc.
+using namespace std;
+
+
+//Variables
+int anInteger = 40643854;
+
+//main
+int main() {
+	cout << "Maximum int value: " << INT_MAX << endl << "Minimum int value: " << INT_MIN << endl;
+	cout << "Size of int: " << sizeof anInteger << endl;
+
+	cin.ignore();
+	return 0;
+}
+
+
+//Functions
+
